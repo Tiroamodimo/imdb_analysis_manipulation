582626dc95ba96a5d67e7aa6bc456f0f330fdc2e
letlhogile <letlhogile@gometroapp.com>
2023-10-24 22:33:12 +0200

    code-update: update DataModel method inputs

--- imdb_data_analysis/app/main.py
+++ imdb_data_analysis/app/main.py
@@ -4,11 +4,13 @@ import streamlit as st
 
 from data_model import DataModel
 
-db_fp = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resources', 'imdb.db')
+queries_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resources', 'queries')
+db_fp = os.path.join(queries_dir, 'imdb.db')
 db_connection = duckdb.connect(db_fp)
 
 top_movies = DataModel() \
-    .get_source_tables(db_connection) \
-    .get_top_movies(db_connection, )
+    .get_source_tables(db_connection, queries_dir) \
+    .get_top_movies(db_connection, queries_dir)
+
 
 st.write(top_movies)
