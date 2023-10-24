1fb4f059042a318b47850ebc8c78415f1e5261fe
letlhogile <letlhogile@gometroapp.com>
2023-10-24 13:59:06 +0200

    re-structure: prepare app for docker deployment

---
+++ imdb_data_analysis/app/resources/queries/transform_top_15_given_100_votes.sql
@@ -0,0 +1,16 @@
+WITH avg_votes AS (
+    SELECT AVG(numVotes) AS average_votes
+    FROM title_ratings
+),
+votes_ratings AS (
+    SELECT tb.primaryTitle, tr.averageRating, tr.numVotes,
+           (tr.numVotes / avg_votes.average_votes) * tr.averageRating AS ranking
+    FROM title_ratings tr
+    JOIN title_basics tb ON tr.tconst = tb.tconst
+    CROSS JOIN avg_votes
+    WHERE tr.numVotes >= 100
+)
+SELECT primaryTitle, numVotes, averageRating, ranking
+FROM votes_ratings
+ORDER BY ranking DESC
+LIMIT 15;
